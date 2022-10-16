8f7f5766eb6c1c5764f830bc75dac00bce90efaa
Mehmet Yalcin <113137811+MehmetYAL@users.noreply.github.com>
2022-10-17 00:10:45 +0300

    Add files via upload

---
+++ Day18_NestedLoop/Stars10.java
@@ -0,0 +1,24 @@
+package Day18_NestedLoop;
+
+public class Stars10 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++){
+            System.out.print("*");
+        System.out.println();
+            for (int j = 1; j < 10; j++) {
+                System.out.print("*");
+//her satirda 10 adet yildiz olacak sekilde yazdirin
+            }}
+
+        }
+    }
+/* *
+**
+***
+****
+*****
+******
+*******
+********
+*********
+********** */
